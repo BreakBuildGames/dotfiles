4c4daab6b79f87459ac92c0d7e90a485e82482bd
Kevin <breakbuildgames@gmail.com>
2024-05-25 12:31:04 +0200

    feat: add new link shortcut

--- .config/nvim/ftplugin/markdown.lua
+++ .config/nvim/ftplugin/markdown.lua
@@ -2,3 +2,9 @@ vim.cmd([[set nofoldenable]])
 vim.opt.textwidth=120
 vim.cmd([[setlocal spell spelllang=en_us]])
 
+-- jump to next header and center it
+vim.keymap.set("n", "<leader>j", "/# <CR>zt", { noremap = false })
+
+-- jump to next header or link
+vim.keymap.set("n", "<leader>n", "/# \\|]]\\|](<CR>", { noremap = false })
+
